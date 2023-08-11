cac68172481ed081fa35651d6d1e054085639cfc
pouriahm44 <140954884+pouriahm44@users.noreply.github.com>
2023-08-11 15:20:08 +0430

    multiples(test) completed

--- multiples.js
+++ multiples.js
@@ -1,3 +1,19 @@
+function Test_Collector() {
+  let c1 = collector(8, 9);
+  if (c1 != 17) {
+    console.log("Failed: Expect = 17, Actual = " + c1);
+  }
+  console.log("Passed: Expected = 17, Actual = " + c1);
+
+  let c2 = collector(2, 3);
+  if (c2 != 38) {
+    console.log("Failed: Expect = 38, Actual = " + c2);
+  }
+  console.log("Passed: Expected = 38, Actual = " + c2);
+}
+
+Test_Collector();
+
 function ismultiples(num) {
   let keep = 0;
   for (let index = 1; index < 10; index++) {
@@ -7,4 +23,6 @@ function ismultiples(num) {
   }
   return keep;
 }
-console.log(ismultiples(5) + ismultiples(3));
+function collector(num1, num2) {
+  return ismultiples(num1) + ismultiples(num2);
+}
