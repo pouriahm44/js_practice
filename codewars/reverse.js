dc0b254fba16ad895e8736a927803b009896e515
pouriahm44 <140954884+pouriahm44@users.noreply.github.com>
2023-08-12 14:46:36 +0430

    practice

---
+++ codewars/reverse.js
@@ -0,0 +1,16 @@
+function reverse(string) {
+  let x = "";
+  if (string.length >= 5) {
+    for (let index = string.length - 1; index >= 0; index--) {
+      x = x + string[index];
+    }
+  }
+  return x;
+}
+//console.log(reverse("Hello world!"));
+function split(para) {
+  for (let index = 0; index < para.length; index++) {
+    if (para[index] == " ") {
+    }
+  }
+}
